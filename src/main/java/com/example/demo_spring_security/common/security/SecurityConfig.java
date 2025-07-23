1c4e9db68c4bcfcdd0ac287f78a324ecb68c1669
JBeanny <yimsotharoth999@gmail.com>
2025-07-23 10:57:11 +0700

    refactor: add STATELESS configuration to spring security

--- src/main/java/com/example/demo_spring_security/common/security/SecurityConfig.java
+++ src/main/java/com/example/demo_spring_security/common/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,7 +16,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authz ->
                         authz
                         .requestMatchers("/api/v1/auth/**").permitAll()
